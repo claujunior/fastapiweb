ddd847bede488700b348ee92d857436d4e5a85e2
claujunior <claudivanjunior2002@hotmail.com>
2026-05-14 00:08:14 -0300

    cors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 
 from routers.user_router import router
-
+from fastapi.middleware.cors import CORSMiddleware
 from exceptions.handler import (
     http_exception_handler
 )
@@ -18,3 +18,10 @@ app.add_exception_handler(
 
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
